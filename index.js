ebee4a3eee3386ff394862decd6763978bb5b5d5
ChrAdrian <chr.adrian@yahoo.com>
2021-12-19 09:37:59 +0200

    + practice

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+let person = {
+    name: 'Adrian',
+    age: 29
+};
+person.age = 25;
+let selection = 'name';
+person[selection] = 'Cristina';
+console.log(person);
+
+let selected_colors = ['red', 'blue'];
+selected_colors[2] = 1;
+console.log(selected_colors.length);
+
+function greet(nume) {
+    console.log('Hello ' + nume)
+}
+
+function square(number) {
+    return number*number;
+}
+
+console.log(square(4));
+greet('Adi');
