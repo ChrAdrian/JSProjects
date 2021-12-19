42deac9c7bc69a25bec930358909493d41314f77
ChrAdrian <chr.adrian@yahoo.com>
2021-12-19 09:52:10 +0200

    + stopwatch

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
-let person = {
-    name: 'Adrian',
-    age: 29
-};
-person.age = 25;
-let selection = 'name';
-person[selection] = 'Cristina';
-console.log(person);
-
-let selected_colors = ['red', 'blue'];
-selected_colors[2] = 1;
-console.log(selected_colors.length);
-
-function greet(nume) {
-    console.log('Hello ' + nume)
-}
-
-function square(number) {
-    return number*number;
-}
-
-console.log(square(4));
-greet('Adi');
+window.onload = function () {
+  
+    var seconds = 00; 
+    var tens = 00; 
+    var appendTens = document.getElementById("tens")
+    var appendSeconds = document.getElementById("seconds")
+    var buttonStart = document.getElementById('button-start');
+    var buttonStop = document.getElementById('button-stop');
+    var buttonReset = document.getElementById('button-reset');
+    var Interval ;
+  
+    buttonStart.onclick = function() {
+      
+      clearInterval(Interval);
+       Interval = setInterval(startTimer, 10);
+    }
+    
+      buttonStop.onclick = function() {
+         clearInterval(Interval);
+    }
+    
+  
+    buttonReset.onclick = function() {
+       clearInterval(Interval);
+      tens = "00";
+        seconds = "00";
+      appendTens.innerHTML = tens;
+        appendSeconds.innerHTML = seconds;
+    }
+    
+     
+    
+    function startTimer () {
+      tens++; 
+      
+      if(tens <= 9){
+        appendTens.innerHTML = "0" + tens;
+      }
+      
+      if (tens > 9){
+        appendTens.innerHTML = tens;
+        
+      } 
+      
+      if (tens > 99) {
+        console.log("seconds");
+        seconds++;
+        appendSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+        appendTens.innerHTML = "0" + 0;
+      }
+      
+      if (seconds > 9){
+        appendSeconds.innerHTML = seconds;
+      }
+    }
+  }
